761293ecb98c3d8f0f121ea1b5ee19431ca2fe9b
EBecerril <ebecerrilrodriguez@outlook.com>
2020-08-16 19:26:52 -0500

    Added main Game Screen

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 
 import Header from './components/Header'
 import StartGameScreen from './screens/StartGameScreen'
+import GameScreen from './screens/GameScreen'
 
 export default function App() {
-    return (
-        <View style={styles.screen}>
-            <Header title='Guess a Number' />
-            <StartGameScreen />
-        </View>
-    )
+  const [userNumber, setUserNumber] = useState()
+  const startGamehandler = selectedNumber => setUserNumber(selectedNumber)
+
+  let content = <StartGameScreen onStartGame={startGamehandler} />
+
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />
+  }
+
+  return (
+    <View style={styles.screen}>
+      <Header title='Guess a Number' />
+      {content}
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-    },
+  screen: {
+    flex: 1,
+  },
 })
